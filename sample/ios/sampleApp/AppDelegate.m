a8d8bc7b2bff2e0fc8111847233539489d19e10f
Will Passidomo <wpassidomo@mparticle.com>
2021-09-21 11:48:03 -0400

    iOS getting close..

--- sample/ios/sampleApp/AppDelegate.m
+++ sample/ios/sampleApp/AppDelegate.m
@@ -11,6 +11,7 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import "mParticle.h"
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -33,8 +34,20 @@ static void InitializeFlipper(UIApplication *application) {
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"sampleApp"
+                                                   moduleName:@"MParticleSample"
                                             initialProperties:nil];
+
+  MParticleOptions *mParticleOptions = [MParticleOptions optionsWithKey:@"REPLACE ME"
+                                                                 secret:@"REPLACE ME"];
+  //Please see the Identity page for more information on building this object
+  MPIdentityApiRequest *request = [MPIdentityApiRequest requestWithEmptyUser];
+  request.email = @"email@example.com";
+  mParticleOptions.identifyRequest = request;
+  mParticleOptions.onIdentifyComplete = ^(MPIdentityApiResult * _Nullable apiResult, NSError * _Nullable error) {
+    NSLog(@"Identify complete. userId = %@ error = %@", apiResult.user.userId, error);
+  };
+  
+  [[MParticle sharedInstance] startWithOptions:mParticleOptions];
 
   if (@available(iOS 13.0, *)) {
       rootView.backgroundColor = [UIColor systemBackgroundColor];
